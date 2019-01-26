a5f6299f20c4a542e9d46d27353715e6883f52e2
Mo Mahmoud <muhamamd.cs@gmail.com>
2019-01-26 18:28:08 +1100

    Add client side with react. Display list of bounties allowing user to add new one

---
+++ client/src/BountyApp.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import BountyNestContract from "./contracts/BountyNest.json";
+import BountyList from "./BountyList";
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import getWeb3 from "./utils/getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+    eth = { web3: null, accounts: null, contract: null };
+    state = { ready: false, bountyList: [], bountiesCount: 0 };
+
+    componentDidMount = async () => {
+        try {
+            // Get network provider and web3 instance.
+            const web3 = await getWeb3();
+
+            // Use web3 to get the user's accounts.
+            const accounts = await web3.eth.getAccounts();
+
+            // Get the contract instance.
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = BountyNestContract.networks[networkId];
+            const instance = new web3.eth.Contract(
+                BountyNestContract.abi,
+                deployedNetwork && deployedNetwork.address,
+            );
+
+            // Set web3, accounts, and contract to the state, and then proceed with an
+            // example of interacting with the contract's methods.
+            this.eth = { web3, accounts, contract: instance };
+            this.setState({ ready: true }, this.readBounties)
+        } catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load web3, accounts, or contract. Check console for details.`,
+            );
+            console.error(error);
+        }
+    };
+
+    readBounties = async () => {
+        try {
+            const { contract } = this.eth;
+
+            const count = await contract.methods.bountiesCount().call();
+
+            // fetch all bounties
+            let bounties = [];
+            for (let i = 1; i <= count; i++) {
+                const bounty = await contract.methods.fetchBounty(i).call();                
+                bounty.id = i;
+                bounties.push(bounty);
+            }
+
+            this.setState({ bountiesCount: count, bountyList: bounties });
+        }
+        catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load bounties.`,
+            );
+            console.error(error);
+        }
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const desc = e.target["description"].value;
+        const reward = e.target["reward"].value;
+
+        await this.addNew(desc, reward);
+    };
+
+    addNew = async (desc, reward) => {
+        try {
+            const { contract, accounts } = this.eth;            
+            const { count } = this.state;
+            const response = await contract.methods.add(desc, reward).send({ from: accounts[0], value: reward + 10, gas: 1000000, gasLimit: 1000000 });
+            console.log(response);
+            this.setState({ bountiesCount: count + 1 }, this.readBounties);
+        } 
+        catch(error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to add bounty.`,
+            );
+            console.error(error);
+        }
+    };
+
+    render() {
+        if (!this.state.ready) {
+            return <div>Loading Web3, accounts, and contract...</div>;
+        }
+        return (
+            <Container>
+                <div className="App">
+                    <h1>Bounties Nest</h1>
+                </div>
+                <Row>
+                    <Col>
+                        <Form onSubmit={this.handleSubmit}>
+                            <fieldset>
+                                <legend>New Bounty</legend>
+                                <Row>
+                                    <Col xs="9">
+                                        <FormGroup>
+                                            <Label for="description">Description</Label>
+                                            <Input type="textarea" id="description"></Input>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col>
+                                        <FormGroup>
+                                            <Label for="reward">Reward</Label>
+                                            <Input type="numeric" id="reward" ></Input>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Button size="lg">Add</Button>
+                                    </Col>
+                                </Row>
+                            </fieldset>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <fieldset>
+                            <legend>List of Bounties ({this.state.bountiesCount})</legend>
+                            <BountyList list={this.state.bountyList}></BountyList>
+                        </fieldset>
+                    </Col>                    
+                </Row>
+            </Container>
+        );
+        // <div className="App">
+        //     <h1>Bounties Nest</h1>
+        //     <div>
+        //         <p>The bounties count is: {this.state.bountiesCount}</p>
+        //     </div>            
+        //     <h3>New Bounty</h3>
+        //     <form className="Submit-New-Bounty">
+
+        //     </form>
+        //     <div className="Submit-New-Bounty">
+        //         <label>Description</label>
+        //         <textarea></textarea> 
+        //         <label>Reward</label>
+        //         <input type="number" />
+        //         <button>Add New Bounty</button>
+        //     </div>
+        //     <h2>Bounties List</h2>
+        //     <BountyList list={ this.state.bountyList }></BountyList>
+        // </div>
+        //);
+    }
+}
+
+export default App;
---
+++ client/src/BountyList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class BountyList extends Component {
+    render() {
+        return (
+            <div className="Bounty-List">
+            {
+                this.props.list.map(bounty => (
+                    <div key={ bounty.id } className="bounty-container">
+                        <div className="bounty-display">
+                            <span>{ bounty.desc }</span> 
+                        </div>
+                        <div className="bounty-reward">
+                            <span className={ bounty.state == "1" ? "bounty-open" : "bounty-closed" }>{ bounty.reward }</span>
+                        </div>                            
+                    </div>
+                ))
+            }
+            </div>          
+        );
+      }    
+}
+
+export default BountyList;
