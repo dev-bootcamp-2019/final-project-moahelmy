c85151389d07de7da58de55ee6b76c9101f440c5
Mohammad Helmy <moahelmy@users.noreply.github.com>
2019-02-05 19:13:29 +1100

    Deploy all contracts instead of only the bounty nest contract and external storage

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-//var Owned = artifacts.require("./Owned.sol");
-//var Admin = artifacts.require("./Admin.sol");
-//var SimpleBank = artifacts.require("./SimpleBank.sol");
+var Owned = artifacts.require("./Owned.sol");
+var Admin = artifacts.require("./Admin.sol");
+var SimpleBank = artifacts.require("./SimpleBank.sol");
 var BountyNest = artifacts.require("./BountyNest.sol");
 var BountyNestStorage = artifacts.require("./BountyNestStorage.sol");
 
 module.exports = function(deployer) {
-    //deployer.deploy(Owned);
-    //deployer.deploy(Admin);
-    //deployer.deploy(SimpleBank);
+    deployer.deploy(Owned);
+    deployer.deploy(Admin);
+    deployer.deploy(SimpleBank);
     deployer.deploy(BountyNestStorage).then(function (){       
         console.log(BountyNestStorage.address);
         return deployer.deploy(BountyNest, BountyNestStorage.address);
